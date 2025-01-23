5f341a762627a17c93eaa7e7f2b6c2bb2e811352
Timiliris_420 <timothydetournay@gmail.com>
2025-01-24 00:37:37 +0100

    1.0.0

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    // Chemin vers les styles prédéfinis
     predefinedStyles: path.resolve(__dirname, 'templates/predefinedStyles.css'),
 
-    // Chemin vers les textures
     texturesPath: path.resolve(__dirname, '../textures'),
 
-    // Exemple : Fonction pour copier les styles et textures dans un projet
     copyStylesAndTextures: function (outputDir) {
         const fs = require('fs-extra'); // Utilise fs-extra pour simplifier la copie
         const stylesPath = path.resolve(__dirname, 'templates/predefinedStyles.css');
         const texturesPath = path.resolve(__dirname, '../textures');
-
-        // Copier les styles
         fs.copySync(stylesPath, path.join(outputDir, 'predefinedStyles.css'));
-
-        // Copier les textures
         fs.copySync(texturesPath, path.join(outputDir, 'textures'));
     },
 };
